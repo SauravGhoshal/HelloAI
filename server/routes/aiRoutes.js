b6dde4f56975c19b2dc1cc7609eef32d47b47b60
Saurav Ghoshal <142157776+SauravGhoshal@users.noreply.github.com>
2026-01-03 02:49:47 +0530

    Fix typo in removeImageObject import

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { auth } from "../middlewares/auth.js";
-import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObejct, resumeReview } from "../controllers/aiController.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
 import { upload } from "../configs/multer.js";
 
 const aiRouter=express.Router();
@@ -10,7 +10,7 @@ aiRouter.post('/generate-image',auth,generateImage)
 aiRouter.post('/remove-image-background',upload.single('image') ,auth,
 removeImageBackground)
 aiRouter.post('/remove-image-object',upload.single('image') ,auth,
-removeImageObejct)
+removeImageObject)
 aiRouter.post('/resume-review',upload.single('resume') ,auth,
 resumeReview)
 
